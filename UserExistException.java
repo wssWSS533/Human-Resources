55aa1800e3b8b34665e18f748416072584db2c10
FilerGX <51429248+FilerGX@users.noreply.github.com>
2019-11-23 01:29:22 +0800

    Add files via upload

---
+++ UserExistException.java
@@ -0,0 +1,29 @@
+package dlnu.hrsys.user.exception;
+
+public class UserExistException extends Exception {
+
+	public UserExistException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserExistException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserExistException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserExistException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserExistException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+}
